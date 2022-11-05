10ba686106b2d62ba3a84e04a706cdb55ea3a9a2
mihail-l-v <mihail.l.v@yandex.ru>
2022-11-05 14:53:13 +0200

    homework-25. localStorage

--- src/js/page-product.js
+++ src/js/page-product.js
@@ -1,4 +1,6 @@
 'use strict';
+window.addEventListener('DOMContentLoaded', init);
+
 const form = document.querySelector('.review-form__form');
 
 const inputName = form.querySelector('.review-form__name');
@@ -6,6 +8,8 @@ const errorNameElem = form.querySelector('.review-form__name-error');
 
 const inputRating = form.querySelector('.review-form__rating');
 const errorRatingElem = form.querySelector('.review-form__rating-error');
+
+const inputReview = form.querySelector('.review-form__text-review');
 
 
 function handleSubmit(event) {
@@ -39,6 +43,29 @@ function handleSubmit(event) {
 
   errorRatingElem.innerText = errorRating;
   errorRatingElem.classList.toggle('review-form__rating-error_visible', errorRating);
+
+  localStorage.removeItem('user-name');
+  localStorage.removeItem('rating');
+  localStorage.removeItem('text-review');
+}
+
+function init() {
+  console.log(`page downloaded`);
+  inputName.value = localStorage.getItem('user-name');
+  inputRating.value = localStorage.getItem('rating');
+  inputReview.value = localStorage.getItem('text-review');
+}
+
+function handleInput(event) {
+  let value = event.target.value;
+  let name = event.target.getAttribute('name');
+  console.log(name, value);
+  localStorage.setItem(name, value);
+  console.log(localStorage.getItem(name));
 }
 
 form.addEventListener('submit', handleSubmit);
+
+inputName.addEventListener('input', handleInput);
+inputRating.addEventListener('input', handleInput);
+inputReview.addEventListener('input', handleInput);
