896950b9628dd5a90695bf209567d7f3f1c4ae1f
mihail-l-v <mihail.l.v@yandex.ru>
2022-11-29 15:17:50 +0200

    homework-33. Add redux for cart

--- react-app/src/components/Header/__basket-counter/HeaderBasketCounter.jsx
+++ react-app/src/components/Header/__basket-counter/HeaderBasketCounter.jsx
@@ -1,8 +1,10 @@
 import './HeaderBasketCounter.scss';
+import { useSelector } from 'react-redux';
 
 function HeaderBasketCounter() {
+  const count = useSelector((state) => state.cart.products.length);
   return (
-    <span className="header__basket-counter">0</span>
+    <span className="header__basket-counter">{count}</span>
   );
 }
 
--- react-app/src/components/PriceMenu/PriceMenu.jsx
+++ react-app/src/components/PriceMenu/PriceMenu.jsx
@@ -1,9 +1,27 @@
 import './_price-menu.scss';
 import './_button.scss';
 import FavoritesButton from '../FavoritesButton/FavoritesButton';
+import { useSelector, useDispatch } from 'react-redux';
+import { addProduct, removeProduct } from './../../reducers/cart-reducer';
 // import FavoritesButtonAdd from '../FavoritesButtonAdd/FavoritesButtonAdd';
 
 function PriceMenu() {
+  const products = useSelector((store) => store.cart.products);
+  const dispatch = useDispatch();
+  const iphone = {id: 4884, name: `Iphone 13`};
+  const hasInCart = products.some((prevProduct) => {
+    return prevProduct.id === iphone.id});
+
+  function handleAddProduct(e, product) {
+    const action = addProduct(product);
+    dispatch(action);
+  };
+
+  function handleDelProduct(e, product) {
+    const action = removeProduct(product);
+    dispatch(action);
+  };
+
   return (
     <div className="price-menu">
       <div className="price-menu__wrapper">
@@ -20,7 +38,11 @@ function PriceMenu() {
       <p className="price-menu__delivery">Самовывоз в четверг, 1 сентября — <span className="price-menu__delivery-bold">бесплатно</span></p>
       <p className="price-menu__delivery">Курьером в четверг, 1 сентября — <span className="price-menu__delivery-bold">бесплатно</span></p>
       <p className="price-menu__button-wrapper">
-        <button className="price-menu__button button button__addCart" type="button">Добавить в корзину</button>
+        {hasInCart ? (
+          <button className="price-menu__button button-dark" onClick={(e) => handleDelProduct(e, iphone)} type="button">Товар уже в корзине</button>
+        ) : (
+          <button className="price-menu__button button button__color" onClick={(e) => handleAddProduct(e, iphone)} type="button">Добавить в корзину</button>
+        )}
       </p>
     </div>
   );
